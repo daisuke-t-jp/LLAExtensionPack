0844750ca18eaee226570f3765260c169d3f840d
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-16 21:56:39 +0900

    Add URL extension test case.

---
+++ LLAExtensionPackTests/LLAExtensionPackTestURL.swift
@@ -0,0 +1,37 @@
+//
+//  LLAExtensionPackTestURL.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2018/12/07.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestURL: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test() {
+
+		let url = URL(string: "https://www.example.com/api?")!
+		XCTAssertTrue(url.LLA.queryMap().keys.isEmpty)
+
+		let url2 = URL(string: "https://www.example.com/api?color=white&time=morning&gender=female")!
+		XCTAssertEqual(url2.LLA.queryMap()["color"], "white")
+		XCTAssertEqual(url2.LLA.queryMap()["time"], "morning")
+		XCTAssertEqual(url2.LLA.queryMap()["gender"], "female")
+		
+	}
+
+}
