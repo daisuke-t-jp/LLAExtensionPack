42544d9edeabe90596b5b177fb7eaf46a633b06b
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-16 21:43:25 +0900

    Add URL extension.

---
+++ LLAExtensionPack/URL+LLA.swift
@@ -0,0 +1,51 @@
+//
+//  URL+LLA.swift
+//  LLAExtensionPack
+//
+//  Created by Daisuke T on 2018/12/07.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import Foundation
+
+
+
+extension URL: LLANamespaceWrapper {}
+public extension LLATypeWrapper where T == URL
+{
+	public func queryMap() -> [String : String]
+	{
+		var res: [String : String] = [String : String]()
+
+		guard let query = SELF.query else
+		{
+			return res
+		}
+
+		let array = query.components(separatedBy: "&")
+	
+		for elm in array
+		{
+			let array2 = elm.components(separatedBy: "=")
+			guard array2.count >= 2 else
+			{
+				continue
+			}
+			
+			guard let key = array2[0].removingPercentEncoding else
+			{
+				continue
+			}
+
+			guard let val = array2[1].removingPercentEncoding else
+			{
+				continue
+			}
+
+			res[key] = val
+		}
+
+		return res
+	}
+	
+}
