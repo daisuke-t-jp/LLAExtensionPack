b70cb83fc78a484763d38c89e6cf30643f925a5f
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-17 20:55:02 +0900

    Import CommonCrypto

--- LLAExtensionPack/LLAExtensionPack.h
+++ LLAExtensionPack/LLAExtensionPack.h
@@ -15,5 +15,6 @@ FOUNDATION_EXPORT double LLAExtensionPackVersionNumber;
 FOUNDATION_EXPORT const unsigned char LLAExtensionPackVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LLAExtensionPack/PublicHeader.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 
