d038722cabb2d05725234a11da6f9c182ae932b7
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-19 12:28:27 +0900

    Add UIDevice extension test.

---
+++ LLAExtensionPackTests/LLAExtensionPackTestUIDevice.swift
@@ -0,0 +1,33 @@
+//
+//  LLAExtensionPackTestUIDevice.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2018/12/07.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestUIDevice: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test() {
+		let major = UIDevice.LLA.systemVersionMajor()
+		let minor = UIDevice.LLA.systemVersionMinor()
+		XCTAssertNotNil(major)
+		XCTAssertNotNil(minor)
+		print("UIDevice test : systemVersion \(major!).\(minor!)")
+	}
+
+}
