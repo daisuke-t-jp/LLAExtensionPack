3f7705ea99544d210d3224886aabb4bdcbe70104
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-19 12:20:44 +0900

    Add UUID extension

---
+++ LLAExtensionPack/Foundation/UUID+LLA.swift
@@ -0,0 +1,21 @@
+//
+//  UUID+LLA.swift
+//  LLAExtensionPack
+//
+//  Created by Daisuke T on 2019/01/17.
+//  Copyright © 2019 Daisuke T. All rights reserved.
+//
+
+import Foundation
+
+
+
+extension UUID: LLANamespaceWrapper {}
+
+public extension LLATypeWrapper where T == UUID
+{
+	static public func string() -> String
+	{
+		return UUID().uuidString
+	}
+}
