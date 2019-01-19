9d9336507bb0c2ba11f6cdb7317cadf1c8f70fc0
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-19 12:25:19 +0900

    Add UUID exntension test.

---
+++ LLAExtensionPackTests/LLAExtensionPackTestUUID.swift
@@ -0,0 +1,29 @@
+//
+//  LLAExtensionPackTestUUID.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2018/12/07.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestUUID: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test() {
+		XCTAssertNotNil(UUID.LLA.string())
+	}
+
+}
