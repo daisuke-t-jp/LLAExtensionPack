7195d289071583bdc58ed98d035d5dd50a1c9bb0
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-19 12:27:11 +0900

    Add UIDevice extension.

---
+++ LLAExtensionPack/UIKit/UIDevice+LLA.swift
@@ -0,0 +1,48 @@
+//
+//  UIDevice+LLA.swift
+//  LLAExtensionPack
+//
+//  Created by Daisuke T on 2018/12/07.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import UIKit
+
+
+
+extension UIDevice: LLANamespaceWrapper {}
+
+public extension LLATypeWrapper where T == UIDevice
+{
+	static private func systemVersionComponents() -> [String]?
+	{
+		let ver = UIDevice.current.systemVersion
+		let array = ver.components(separatedBy: ".")
+		guard array.count >= 2 else
+		{
+			return nil
+		}
+		
+		return array
+	}
+
+	static public func systemVersionMajor() -> Int?
+	{
+		guard let array = systemVersionComponents() else
+		{
+			return nil
+		}
+
+		return Int(array[0])
+	}
+
+	static public func systemVersionMinor() -> Int?
+	{
+		guard let array = systemVersionComponents() else
+		{
+			return nil
+		}
+		
+		return Int(array[1])
+	}
+}
