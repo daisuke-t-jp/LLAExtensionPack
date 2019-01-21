e634206f43e11808f8a83ba5d2cf10dc3b2eb6b6
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-21 23:14:33 +0900

    Add CoreLocation test

---
+++ LLAExtensionPackTests/CoreLocation/LLAExtensionPackTestCLBeacon.swift
@@ -0,0 +1,30 @@
+//
+//  LLAExtensionPackTestCLBeacon.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2019/01/21.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestCLBeacon: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+	func test() {
+		// NOP
+	}
+
+}
---
+++ LLAExtensionPackTests/CoreLocation/LLAExtensionPackTestCLLocationManager.swift
@@ -0,0 +1,40 @@
+//
+//  LLAExtensionPackTestCLLocationManager.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2019/01/21.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestCLLocationManager: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+	func test() {
+		XCTAssertEqual(CLLocationManager.LLA.authorizationStatus2String(.notDetermined), "notDetermined")
+		XCTAssertEqual(CLLocationManager.LLA.authorizationStatus2String(.restricted), "restricted")
+		XCTAssertEqual(CLLocationManager.LLA.authorizationStatus2String(.denied), "denied")
+		XCTAssertEqual(CLLocationManager.LLA.authorizationStatus2String(.authorizedAlways), "authorizedAlways")
+		XCTAssertEqual(CLLocationManager.LLA.authorizationStatus2String(.authorizedWhenInUse), "authorizedWhenInUse")
+
+		XCTAssertFalse(CLLocationManager.LLA.isAuthorizationStatusWhenInUseOrAlways(.notDetermined))
+		XCTAssertFalse(CLLocationManager.LLA.isAuthorizationStatusWhenInUseOrAlways(.restricted))
+		XCTAssertFalse(CLLocationManager.LLA.isAuthorizationStatusWhenInUseOrAlways(.denied))
+		XCTAssertTrue(CLLocationManager.LLA.isAuthorizationStatusWhenInUseOrAlways(.authorizedAlways))
+		XCTAssertTrue(CLLocationManager.LLA.isAuthorizationStatusWhenInUseOrAlways(.authorizedWhenInUse))
+	}
+
+}
