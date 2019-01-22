9df5c96d3bce78efa4f4bd39993b2541f04c6070
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-22 21:21:48 +0900

    Add BinaryInteger extension

---
+++ LLAExtensionPack/Foundation/BinaryInteger+LLA.swift
@@ -0,0 +1,30 @@
+//
+//  BinaryInteger+LLA.swift
+//  LLAExtensionPack
+//
+//  Created by Daisuke T on 2019/01/22.
+//  Copyright © 2019 Daisuke T. All rights reserved.
+//
+
+import Foundation
+
+
+
+// MARK: WITHOUT-NAMESPACE
+public extension BinaryInteger
+{
+	public var radianDouble: Double
+	{
+		return Double(self) * .pi / 180
+	}
+
+	public var radianFloat: Float
+	{
+		return Float(self) * .pi / 180
+	}
+
+	public var radianCGFloat: CGFloat
+	{
+		return CGFloat(self) * .pi / 180
+	}
+}
