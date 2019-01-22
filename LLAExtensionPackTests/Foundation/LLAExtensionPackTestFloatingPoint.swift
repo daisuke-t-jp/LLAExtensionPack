da9eb4db1c2cafd6e00ec060bd84716b1a7547eb
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-01-22 21:27:59 +0900

    Add FloatingPoint test

---
+++ LLAExtensionPackTests/Foundation/LLAExtensionPackTestFloatingPoint.swift
@@ -0,0 +1,30 @@
+//
+//  LLAExtensionPackTestFloatingPoint.swift
+//  LLAExtensionPackTests
+//
+//  Created by Daisuke T on 2018/01/22.
+//  Copyright © 2018 Daisuke T. All rights reserved.
+//
+
+import XCTest
+
+@testable import LLAExtensionPack
+
+
+
+class LLAExtensionPackTestFloatingPoint: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test() {
+		XCTAssertEqual(123.456.string[...6], "123.456")
+		XCTAssertEqual(180.radian.string[...3], "3.14")
+		XCTAssertEqual(3.14.degree.rounded().string[...2], "180")
+	}
+}
